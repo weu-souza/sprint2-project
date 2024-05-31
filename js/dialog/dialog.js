b417c63cbfabe1a0e2b6803606d7a090a908255e
weu-souza <weuller1561@gmail.com>
2024-05-31 17:43:28 -0300

    feat-dialog: first modal ended

--- js/dialog/dialog.js
+++ js/dialog/dialog.js
@@ -1,2 +1,29 @@
-let button = document.querySelector(".btn-donate");
+let btnDiag = document.querySelectorAll(".btn-donate");
+let btnAdp = document.querySelectorAll(".adopt-click");
+let terms = document.getElementById("agree-terms");
+let labelRadio = document.querySelector(".agre-container label");
 
+
+// let sucessPageBtn = document.querySelectorAll('.success-page-navig')
+
+export function diag() {
+  for (let i = 0; i < btnDiag.length; i++) {
+    btnDiag[i].addEventListener("click", () => {
+      console.log("clicou");
+    });
+  }
+
+  for (let j = 0; j < btnAdp.length; j++) {
+    btnAdp[j].addEventListener("click", () => {
+      console.log("adpt");
+    });
+  }
+
+  terms.addEventListener("click", () => {
+    if (terms.checked) {
+      labelRadio.style.backgroundColor = "#04C27F";
+    } else {
+      labelRadio.style.backgroundColor = "#37383f";
+    }
+  });
+}
--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 import { load, resize } from "./main-screen/Photo.js";
 import { nextButton, prevButton } from "./main-screen/carousel.js";
-import {} from "./dialog/dialog.js";
+import { diag } from "./dialog/dialog.js";
 
+diag();
 load;
 resize;
 prevButton;
 nextButton;
+
+
+
+
+
+
+
