98f570fd4420b60d14dbe6f903a2ce93ac2d55a5
weu-souza <weuller1561@gmail.com>
2024-05-31 18:40:46 -0300

    feat-dialog:modal adopt js works

--- js/dialog/dialog.js
+++ js/dialog/dialog.js
@@ -1,21 +1,20 @@
-let btnDiag = document.querySelectorAll(".btn-donate");
-let btnAdp = document.querySelectorAll(".adopt-click");
+let donate = document.querySelectorAll(".btn-donate");
+let adopt = document.querySelectorAll(".adopt-click");
 let terms = document.getElementById("agree-terms");
 let labelRadio = document.querySelector(".agre-container label");
-
+let dialogAdapt = document.querySelector(".dialog-adapt-container");
+let btnClosedAdapt = document.querySelector(".modal-cancel");
 
 // let sucessPageBtn = document.querySelectorAll('.success-page-navig')
 
 export function diag() {
-  for (let i = 0; i < btnDiag.length; i++) {
-    btnDiag[i].addEventListener("click", () => {
-      console.log("clicou");
-    });
+  for (let i = 0; i < donate.length; i++) {
+    donate[i].addEventListener("click", () => {});
   }
 
-  for (let j = 0; j < btnAdp.length; j++) {
-    btnAdp[j].addEventListener("click", () => {
-      console.log("adpt");
+  for (let j = 0; j < adopt.length; j++) {
+    adopt[j].addEventListener("click", () => {
+      showDialogAdapt();
     });
   }
 
@@ -26,4 +25,24 @@ export function diag() {
       labelRadio.style.backgroundColor = "#37383f";
     }
   });
+
+  btnClosedAdapt.addEventListener("click", () => {
+    closeDialogAdapt();
+  });
 }
+
+function showDialogAdapt() {
+  dialogAdapt.classList.remove("dialog-closed");
+  dialogAdapt.classList.remove("hide");
+  dialogAdapt.classList.add("dialog-open");
+}
+
+function closeDialogAdapt() {
+  setTimeout(() => {
+    dialogAdapt.classList.add("hide");
+  }, 400);
+  dialogAdapt.classList.remove("dialog-open");
+  dialogAdapt.classList.add("dialog-closed");
+}
+
+function hide() {}
