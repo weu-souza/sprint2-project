ab4aa28c41e5ee2363a9207fc2b9a430b965696b
weu-souza <weuller1561@gmail.com>
2024-06-03 17:54:40 -0300

    feat-other: carousel js ended

--- js/index.js
+++ js/index.js
@@ -1,28 +1,8 @@
-import { nextButton, prevButton } from "./main-screen/carousel.js";
+import { carouselBtns } from "./main-screen/carousel.js";
 import { diag } from "./dialog/dialog.js";
 
 diag();
-prevButton;
-nextButton;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// prevent forms updating page
+carouselBtns();
 let form = document.querySelectorAll("form");
 for (let i = 0; i < form.length; i++) {
   form[i].addEventListener("submit", (event) => {
--- js/main-screen/carousel.js
+++ js/main-screen/carousel.js
@@ -1,70 +1,77 @@
-// code for carousel
-
 let index = 0;
 let btnPrev = document.getElementById("button-left-carousel");
 let btnNext = document.getElementById("button-right-carousel");
-
 let dots = document.querySelectorAll(".dots-container #dots");
 let imgsCarrossel = document.querySelectorAll("#imgs-carousel img");
-
-let imgs = [
-  "img/carousel-imgs/1.png",
-  "img/carousel-imgs/2.png",
-  "img/carousel-imgs/3.png",
-  "img/carousel-imgs/4.png",
-  "img/carousel-imgs/5.png",
+export function carouselBtns() {
+  btnNext.addEventListener("click", () => {
+    if (index === dots.length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    for (let i = 0; i <= dots.length; i++) {
+      if (i === index) {
+        dots.forEach((j) => j.classList.remove("dots-active"));
+        dots.forEach((j) => j.classList.remove("slide-right"));
+        dots[index].classList.add("slide-right");
+        dots[index].classList.add("dots-active");
+        changeImg(i);
+      }
+    }
+  });
+  btnPrev.addEventListener("click", () => {
+    if (index === 0) {
+      index = dots.length - 1;
+    } else {
+      index--;
+    }
+    for (let i = dots.length - 1; i > -1; i--) {
+      if (i === index) {
+        dots.forEach((j) => j.classList.remove("dots-active"));
+        dots.forEach((j) => j.classList.remove("slide-left"));
+        dots[index].classList.add("slide-left");
+        dots[index].classList.add("dots-active");
+        changeImg(i);
+      }
+    }
+  });
+}
+const imgs = [
+  {
+    um: "img/carousel-imgs/1.png",
+    dois: "img/carousel-imgs/2.png",
+    tres: "img/carousel-imgs/3.png",
+    quatro: "img/carousel-imgs/4.png",
+  },
+  {
+    um: "img/carousel-imgs/2.png",
+    dois: "img/carousel-imgs/3.png",
+    tres: "img/carousel-imgs/4.png",
+    quatro: "img/carousel-imgs/5.png",
+  },
+  {
+    um: "img/carousel-imgs/3.png",
+    dois: "img/carousel-imgs/4.png",
+    tres: "img/carousel-imgs/5.png",
+    quatro: "img/carousel-imgs/1.png",
+  },
+  {
+    um: "img/carousel-imgs/4.png",
+    dois: "img/carousel-imgs/5.png",
+    tres: "img/carousel-imgs/1.png",
+    quatro: "img/carousel-imgs/2.png",
+  },
+  {
+    um: "img/carousel-imgs/5.png",
+    dois: "img/carousel-imgs/1.png",
+    tres: "img/carousel-imgs/2.png",
+    quatro: "img/carousel-imgs/3.png",
+  },
 ];
-
-export const nextButton = btnNext.addEventListener("click", () => {
-  if (index === dots.length - 1) {
-    index = 0;
-  } else {
-    index++;
-  }
-  for (let i = 0; i <= dots.length; i++) {
-    if (i === index) {
-      dots.forEach((j) => j.classList.remove("dots-active"));
-      dots.forEach((j) => j.classList.remove("slide-right"));
-      dots[index].classList.add("slide-right");
-      dots[index].classList.add("dots-active");
-
-      changeImg();
-    }
-  }
-});
-
-export const prevButton = btnPrev.addEventListener("click", () => {
-  if (index === 0) {
-    index = dots.length - 1;
-  } else {
-    index--;
-  }
-  for (let i = dots.length - 1; i > -1; i--) {
-    if (i === index) {
-      dots.forEach((j) => j.classList.remove("dots-active"));
-      dots.forEach((j) => j.classList.remove("slide-left"));
-      dots[index].classList.add("slide-left");
-      dots[index].classList.add("dots-active");
-      changeImg();
-    }
-  }
-});
-
-const changeImg = () => {
-  imgsCarrossel[0].setAttribute(
-    "src",
-    imgs[Math.floor(Math.random() * imgs.length)]
-  );
-  imgsCarrossel[1].setAttribute(
-    "src",
-    imgs[Math.floor(Math.random() * imgs.length)]
-  );
-  imgsCarrossel[2].setAttribute(
-    "src",
-    imgs[Math.floor(Math.random() * imgs.length)]
-  );
-  imgsCarrossel[3].setAttribute(
-    "src",
-    imgs[Math.floor(Math.random() * imgs.length)]
-  );
+const changeImg = (i) => {
+  imgsCarrossel[0].setAttribute("src", imgs[i].um);
+  imgsCarrossel[1].setAttribute("src", imgs[i].dois);
+  imgsCarrossel[2].setAttribute("src", imgs[i].tres);
+  imgsCarrossel[3].setAttribute("src", imgs[i].quatro);
 };
