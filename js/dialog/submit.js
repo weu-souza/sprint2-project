fa084de2c97ee2b713a929b7a9fecfba10ad6be3
weu-souza <weuller1561@gmail.com>
2024-06-03 22:05:59 -0300

    form-issues: solving some issues in form submit

--- js/dialog/submit.js
+++ js/dialog/submit.js
@@ -1,17 +1,9 @@
-const donate = document.querySelectorAll("#form-submit");
+const form = document.querySelectorAll("form");
 export function submit() {
-  preventPageReloadOnSubmit();
-  for (let i = 0; i < donate.length; i++) {
-    donate[i].addEventListener("submit", () => {
-        window.location.href = "/pages/success.html";
-      });
-    
-  }
-}
-function preventPageReloadOnSubmit() {
-  const form = document.querySelectorAll("form");
   for (let i = 0; i < form.length; i++) {
     form[i].addEventListener("submit", (event) => {
+      // window.location.href = "/pages/success.html";
+      console.log((window.location.href = "pages/success.html"));
       event.preventDefault();
     });
   }
